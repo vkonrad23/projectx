d9b708a7161f8378751677b98f5eae0171211c4e
konradv <vladyslav.konrad@auk.edu.ua>
2023-08-29 15:44:40 +0300

    java project

--- src/Main.java
+++ src/Main.java
@@ -5,13 +5,18 @@ public class Main {
         Scanner keyboard = new Scanner(System.in);
         String name;
         int age;
-
         System.out.println("What is your name?");
         name = keyboard.nextLine();
+        boolean isNameVlad = name.contains("Vlad");
+
+        if(isNameVlad)
+        {
+            System.out.println("I was waiting for you, Vlad!");
+        }
 
         System.out.println("What is your age?");
         age = keyboard.nextInt();
         System.out.println("Hello, " + name);
-        System.out.println("Damn, you're " + age);
+        System.out.println("Damn, you're "  + age);
     }
 }
